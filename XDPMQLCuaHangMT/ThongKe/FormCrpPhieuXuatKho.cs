7618b3d4f4df1e4ef93c9c41792090bb7c6adc98
tim <levieitien5744@gmail.com>
2026-01-03 18:56:18 +0700

    Add Crystal Reports for warehouse statistics
    Introduced CrystalReportPhieuNhapKho, CrystalReportPhieuXuatKho, and CrystalReportTongHopTonKho along with their designer, code-behind, and resource files for reporting. Updated FormFinalStock to use new report forms and extended PhieuNhapKho dataset to support period-based stock statistics.

---
+++ XDPMQLCuaHangMT/ThongKe/FormCrpPhieuXuatKho.cs
@@ -0,0 +1,42 @@
+﻿using BUS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XDPMQLCuaHangMT.ThongKe
+{
+    public partial class FormCrpPhieuXuatKho : Form
+    {
+        public FormCrpPhieuXuatKho()
+        {
+            InitializeComponent();
+        }
+        public FormCrpPhieuXuatKho(int id)
+        {
+            InitializeComponent();
+            this.voucherId__ = id;
+        }
+        protected int voucherId__;
+        BUS_ThongKePhieu busThongKePhieu = new BUS_ThongKePhieu();
+        private void FormCrpPhieuXuatKho_Load(object sender, EventArgs e)
+        {
+            this.crystalReportViewer1.Refresh();
+            Load_Report();
+        }
+        private void Load_Report()
+        {
+            DataTable dt = busThongKePhieu.GetVoucherDetailsByVoucherId(voucherId__);
+
+            CrystalReportPhieuXuatKho rpt = new CrystalReportPhieuXuatKho();
+            rpt.SetDataSource(dt);
+            crystalReportViewer1.ReportSource = rpt;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
